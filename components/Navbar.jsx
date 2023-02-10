448b35b26bce77123707c7156313cc21738e0965
ItsKalfar <gundsagar02@gmail.com>
2023-02-11 00:25:05 +0530

    able to go to login page & unable to access email

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,36 +1,103 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { SiLichess } from "react-icons/si";
 import { OAuth2AuthCodePKCE } from "@bity/oauth2-auth-code-pkce";
 import { toast } from "react-hot-toast";
-import axios from "axios";
-import queryString from "query-string";
 
 export default function Navbar() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const lichessHost = "https://lichess.org";
-  const clientId = "http://localhost:3000/";
-  const clientUrl = `http://localhost:3000/logged`;
-  const scopes = ["email:read"];
+  const [email, setEmail] = useState(null);
 
-  const handleLogin = async () => {
-    const auth = new OAuth2AuthCodePKCE({
+  useEffect(() => {
+    const checkAuthCode = async () => {
+      const lichessHost = `https://lichess.org`;
+      const clientId = "http://localhost:3000/";
+      const scopes = ["email:read"];
+      const clientUrl = "http://localhost:3000/logged";
+      const oauth = new OAuth2AuthCodePKCE({
+        authorizationUrl: `${lichessHost}/oauth`,
+        tokenUrl: `${lichessHost}/api/token`,
+        clientId,
+        scopes,
+        redirectUrl: clientUrl,
+        onAccessTokenExpiry: (refreshAccessToken) => refreshAccessToken(),
+        onInvalidGrant: (error) => toast.error(error),
+      });
+      try {
+        const hasAuthCode = await oauth.isReturningFromAuthServer();
+        if (hasAuthCode) {
+          accessContext = await oauth.getAccessToken();
+          if (typeof window !== "undefined") {
+            const fetch = oauth.decorateFetchHTTPClient(window.fetch);
+            const res = await fetch(`${lichessHost}/api/account`);
+            const userEmail = { ...(await res.json()), httpClient };
+            toast.success(userEmail);
+            setEmail(userEmail);
+          }
+        }
+      } catch (error) {
+        toast.error(error);
+      }
+    };
+    checkAuthCode();
+    toast.success("Run");
+  }, [email]);
+
+  const handleLogIn = async () => {
+    const lichessHost = `https://lichess.org`;
+    const clientId = "http://localhost:3000/";
+    const scopes = ["email:read"];
+    const clientUrl = "http://localhost:3000/logged";
+    const oauth = new OAuth2AuthCodePKCE({
       authorizationUrl: `${lichessHost}/oauth`,
       tokenUrl: `${lichessHost}/api/token`,
       clientId,
       scopes,
       redirectUrl: clientUrl,
-      onAccessTokenExpiry: (refreshAccessToekn) => refreshAccessToekn(),
-      onInvalidGrant: console.warn,
+      onAccessTokenExpiry: (refreshAccessToken) => refreshAccessToken(),
+      onInvalidGrant: (error) => toast.error(error),
     });
 
+    await oauth.fetchAuthorizationCode();
     try {
-      await auth.fetchAuthorizationCode();
-      setIsLoggedIn(true);
-      toast.success("Done");
+      const accessContext = await oauth.getAccessToken();
+      if (accessContext) {
+        if (typeof window !== "undefined") {
+          const fetch = oauth.decorateFetchHTTPClient(window.fetch);
+          const res = await fetch(`${lichessHost}/api/account`);
+          const userEmail = { ...(await res.json()), httpClient };
+          toast.success(userEmail);
+          setEmail(userEmail);
+        }
+      }
     } catch (error) {
       toast.error(error);
     }
+
+    if (!email) {
+      try {
+        const hasAuthCode = await oauth.isReturningFromAuthServer();
+        if (hasAuthCode) {
+          if (typeof window !== "undefined") {
+            const fetch = oauth.decorateFetchHTTPClient(window.fetch);
+            const res = await fetch(`${lichessHost}/api/account`);
+            const userEmail = { ...(await res.json()), httpClient };
+            toast.success(userEmail);
+            setEmail(userEmail);
+          }
+        }
+      } catch (error) {
+        toast.error(error);
+      }
+    }
+  };
+  const handleLogOut = async () => {
+    if (email) {
+      await email.httpClient(`${lichessHost}/api/token`, { method: "DELETE" });
+      localStorage.clear();
+      setEmail(null);
+    }
+    setIsLoggedIn(false);
   };
 
   return (
@@ -45,17 +112,18 @@ export default function Navbar() {
         <Link href="/leaderboard" className="nav-link">
           Leaderboard
         </Link>
-        {isLoggedIn ? (
-          <button className="btn btn-yellow" onClick={handleLogin}>
-            <p>Log Out</p>
+        {email ? (
+          <button className="btn btn-yellow" onClick={handleLogOut}>
+            <span>Log out</span>
             <SiLichess className="Silichess" />
           </button>
         ) : (
-          <button className="btn btn-yellow" onClick={handleLogin}>
-            <p>Login with</p>
+          <button className="btn btn-yellow" onClick={handleLogIn}>
+            <span>Login with</span>
             <SiLichess className="Silichess" />
           </button>
         )}
+        <div className="user-profile">{email}</div>
       </div>
     </nav>
   );
